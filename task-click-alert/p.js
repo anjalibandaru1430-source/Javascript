8820a574264f3ad7fa818ebbe1e1a943301ac21c
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-03-21 22:06:17 +0530

    js

--- task-click-alert/p.js
+++ task-click-alert/p.js
@@ -1,5 +1,5 @@
 function display_Data(){
-    //alert('Test Case 123')
+    alert('Test Case 123')
     //console.log("Test Case 123")
     //document.writeln("Test Case 123")
     let pTag=document.getElementsByTagName('p')[0]
