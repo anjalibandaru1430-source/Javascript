1a7dceb49c5f05547470b5878369d6dcd9497db9
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-03-24 11:45:39 +0530

    js

---
+++ class-15-fatarrow/great.js
@@ -0,0 +1,17 @@
+/*function add(){
+   return a+b;
+}
+let r1=add(10,20)
+console.log(r1)*/
+
+///Fat arrow
+let add=(a,b)=>{
+    return a+b;
+}
+let r2=add(10,20)
+console.log(r2);
+
+/*implict return
+let add=(a,b)=>a+b;
+let r3=add(20,10)
+console.log(r1)*/
---
+++ class-15-fatarrow/this.js
@@ -0,0 +1,3 @@
+function wish(name){
+    return "HEllo:"+name
+}
