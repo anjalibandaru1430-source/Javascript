64bf8963e6645734f42ad0f8b24555005f94908f
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-06-04 22:58:38 +0530

    js'

---
+++ Practice/Divisible7.js
@@ -0,0 +1,10 @@
+function divisibleBy7(){
+    if(num%7==0){
+        console.log("num is divisible by 7");
+    }
+    else{
+        console.log("num is not divisible by 7");
+    }
+}
+let num=49;
+divisibleBy7();
---
+++ Practice/greatest.js
@@ -0,0 +1,15 @@
+function greatest(){
+    if(a>b && a>c){
+        console.log("a is greatest");
+    }
+    else if(b>c){
+        console.log("b is greatest");
+    }
+    else{
+        console.log("c is greatest");
+    }
+}
+let a = 10;
+let b = 20;
+let c = 30;
+greatest()
